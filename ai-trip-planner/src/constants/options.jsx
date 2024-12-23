111ba0e1ffc863aa5983c203fffce3aad5d3ab81
Jimil Soni <jimilsoni926@gmail.com>
2024-12-23 16:05:22 +0530

    Added constants for shared configuration and static values

---
+++ ai-trip-planner/src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelesList = [
+    {
+      id: 1,
+      title: 'Just Me',
+      desc: 'A sole traveler in exploration',
+      icon: '+',
+      people: '1'
+    },
+    {
+      id: 2,
+      title: 'A Couple',
+      desc: 'Two travelers in tandem',
+      icon: '👫',
+      people: '2 People'
+    },
+    {
+      id: 3,
+      title: 'Family',
+      desc: 'A group of fun loving adv',
+      icon: '👪',
+      people: '3 to 5 People'
+    },
+    {
+        id: 4,
+        title: 'Friends',
+        desc: 'A bunch of thrill-seekers',
+        icon: '🔥',
+        people: '5 to 10 People'
+      },
+  ];
+
+  export const SelectBudgetOptions = [
+    {
+      id: 1,
+      title: 'Cheap',
+      desc: 'Stay conscious of costs',
+      icon: '💸'
+    },
+    {
+      id: 2,
+      title: 'Moderate',
+      desc: 'Keep cost on the average side',
+      icon: '💰'
+    },
+    {
+        id: 3,
+        title: 'Luxury',
+        desc: 'Dont worry about cost',
+        icon: '💎'
+      },
+      
+    ];
+export const AI_PROMPT = 'Generate Travel Plan for Location : {location},for {totalDays} for {traveler} with a {budget} budget, Give me a Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates, ticket Pricing, rating, Time travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.';
